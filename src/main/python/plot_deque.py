664971186133f30f4a28f9a510da062b1b6a65d8
ycqian <1767855130@qq.com>
2021-05-14 00:43:40 +0800

    fix py script

--- src/main/python/plot_deque.py
+++ src/main/python/plot_deque.py
@@ -2,7 +2,7 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-tasks_str = ["All Read", "All Write", "Most Read", "Equal R&W", "Most Write"]
+tasks_str = ["All Write", "All Read", "Most Read", "Equal R&W", "Most Write"]
 impls_str = ["Std", "Coarse", "Fine", "RW", "CAS", "Ticket"]
 
 tasks_count = 5
@@ -10,6 +10,7 @@ impls_count = 6
 
 
 def draw_perf(input_data, task_id):
+    print(input_data)
     base = [1, 2, 4, 8, 16]
 
     for i in range(impls_count):
@@ -17,7 +18,8 @@ def draw_perf(input_data, task_id):
     # plotting the line 2 points
     plt.xlabel('thread num')
     # Set the y axis label of the current axis.
-    plt.ylabel('relative perf')
+    plt.ylabel('throughput ops/ms')
+    plt.yscale('log')
     plt.title("%s task stat.png" % (tasks_str[task_id]))
     # show a legend on the plot
     plt.legend()
@@ -30,8 +32,9 @@ def draw_perf(input_data, task_id):
 if __name__ == "__main__":
     """
     # ensure a total of 30 lines for inputs.
-    # Every 5 lines belong to an implementation
-    # There are a total of 6 impls
+    # Every 6 lines belong to a task
+    # Each task has 6task_id implementations
+    # There are a total of 5task_id tasks
     # Each columns represents different thread nums, 1, 2, 4, 8, 16
     # The impls should be arranged in the following order ["Std", "Coarse", "Fine", "RW", "CAS", "Ticket"]
     """
@@ -39,6 +42,7 @@ if __name__ == "__main__":
     input = np.genfromtxt(in_file, delimiter=' ', dtype=np.float64, encoding='UTF-8')
 
     for task_id in range(tasks_count):
-        index = [task_id + impl * tasks_count for impl in range(6)]
+        index = [x for x in range(task_id * 6, task_id * 6 + 6)]
+        print(index)
         draw_perf(input[index].copy(), task_id)
 
